8610b0c1f0807676dea90ce195fc54c69f3b009c
Chris Stiles <chrisstiles92@gmail.com>
2020-03-31 09:55:13 -0700

    Add basic text clipping

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -8,8 +8,40 @@ export default function Home() {
     <Layout>
       <SEO title="Home" />
       <section className={styles.hero}>
-        <h1>Chris Stiles</h1>
+        <div className={styles.content}>
+          {/* <h1>Chris Stiles</h1> */}
+          <SVGClip />
+        </div>
       </section>
     </Layout>
+  );
+}
+
+function SVGClip() {
+  return (
+    <div className={styles.clip}>
+      <div className={styles.bgWrapper}>
+        <div className={styles.bg} />
+      </div>
+      <svg viewBox="0 0 900 500">
+        <defs>
+          <clipPath id="text-clip">
+            <text x="50" y="133" fontWeight="900" fontSize="100px">
+              Chris Stiles
+            </text>
+            {/* <rect x="100" y="400" width="250" height="250" /> */}
+          </clipPath>
+        </defs>
+
+        {/* <rect
+          className={styles.rect}
+          x="0"
+          y="0"
+          width="900"
+          height="500"
+          clipPath="url(#text-clip)"
+        /> */}
+      </svg>
+    </div>
   );
 }
