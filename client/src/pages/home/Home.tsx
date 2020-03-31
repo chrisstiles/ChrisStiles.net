710310a14dccfcf1a9dfcda73f409f675e3348d0
Chris Stiles <chrisstiles92@gmail.com>
2020-03-31 14:10:00 -0700

    Add fixed element overlay mask

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import Layout from '@components/Layout';
 import SEO from '@components/SEO';
 import styles from './Home.module.scss';
 import space from './space.jpg';
 import classNames from 'classnames';
+import gsap from 'gsap';
 
 export default function Home() {
   return (
@@ -13,6 +15,14 @@ export default function Home() {
         <div className={styles.content}>
           {/* <h1>Chris Stiles</h1> */}
           <SVGClip />
+          <svg width="100%" height="100%" className={styles.overlay}>
+            <rect
+              width="100%"
+              height="100%"
+              fill="#fff"
+              mask="url(#overlay-clip)"
+            />
+          </svg>
         </div>
       </section>
     </Layout>
@@ -20,24 +30,58 @@ export default function Home() {
 }
 
 function SVGClip() {
+  const rectRef = useRef<SVGRectElement>(null);
+
+  useEffect(() => {
+    console.log(rectRef.current);
+  }, []);
+
   return (
     <React.Fragment>
+      {/* <div className={styles.clip}>
+
+      </div> */}
+
       <div className={styles.clip}>
-        <div className={styles.bgWrapper}>
+        {/* <div className={styles.bgWrapper}>
           <div className={styles.bg} />
-        </div>
-        <svg width="0" height="0">
+        </div> */}
+        <svg width="100%" height="100%">
           <defs>
-            <clipPath id="text-clip">
-              <text x="50" y="133" fontWeight="900" fontSize="100px">
-                HTML Clip
+            <mask id="overlay-clip">
+              <rect x="0" y="0" width="100%" height="100%" fill="#FFF" />
+              <text
+                x="50"
+                y="133"
+                fontWeight="900"
+                fontSize="100px"
+                fill="#000"
+              >
+                Overlay Clip
               </text>
-            </clipPath>
+
+              {/* <rect
+                x="700"
+                y="10"
+                // width="100"
+                // height="100"
+                className={styles.rectAnimation}
+              /> */}
+            </mask>
           </defs>
         </svg>
+
+        {/* <svg width="100%" height="100%" className={styles.overlay}>
+          <rect
+            width="100%"
+            height="100%"
+            fill="#fff"
+            mask="url(#overlay-clip)"
+          />
+        </svg> */}
       </div>
 
-      <div
+      {/* <div
         className={classNames(styles.clip, styles.bgAnimation)}
         style={{
           backgroundImage: `url(${space})`,
@@ -58,6 +102,16 @@ function SVGClip() {
               >
                 SVG Clip
               </text>
+
+              <rect
+                x="700"
+                y="10"
+                fill="#000"
+                width="200px"
+                height="500px"
+                ref={rectRef}
+                className={styles.rectAnimation}
+              />
             </mask>
           </defs>
 
@@ -70,7 +124,17 @@ function SVGClip() {
             mask="url(#svg-clip)"
           />
         </svg>
-      </div>
+      </div> */}
+      <FixedBackground />
     </React.Fragment>
+  );
+}
+
+function FixedBackground() {
+  // return ReactDOM.createPortal(<div className={styles.bg} />, document.body)
+  return (
+    <div className={styles.fixedBgWraooer}>
+      <div className={styles.fixedBg} />
+    </div>
   );
 }
