22adf8072f9744db334e5dfc737b74eb4918d7ee
Chris Stiles <chrisstiles92@gmail.com>
2020-04-01 00:41:22 -0700

    Remove older clipping methods

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -8,55 +8,40 @@ import gsap from 'gsap';
 
 export default function Home() {
   return (
-    <Layout>
-      <SEO title="Home" />
-      <section className={styles.hero}>
-        <div className={styles.content}>
-          {/* <h1>Chris Stiles</h1> */}
-          <SVGClip />
-        </div>
-      </section>
+    <React.Fragment>
+      <Layout>
+        <SEO title="Home" />
+        <section className={styles.hero}>
+          <div className={styles.content}>
+            <h1>Chris Stiles</h1>
+            <SVGClip />
+          </div>
+        </section>
+      </Layout>
       <FixedBackground />
-    </Layout>
+    </React.Fragment>
   );
 }
 
 function SVGClip() {
   const rectRef = useRef<SVGRectElement>(null);
 
-  useEffect(() => {
-    console.log(rectRef.current);
-  }, []);
+  // useEffect(() => {
+  //   console.log(rectRef.current);
+  // }, []);
+
+  // console.log('Here');
 
   return (
     <React.Fragment>
-      {/* <div className={styles.clip}>
-
-      </div> */}
-
       <div className={styles.clip}>
-        {/* <div className={styles.bgWrapper}>
-          <div className={styles.bg} />
-        </div> */}
         <svg width="100%" height="300px">
           <defs>
             <mask id="overlay-mask">
               <rect x="0" y="0" width="100%" height="100%" fill="#FFF" />
               <text x="0" y="80" fontWeight="900" fontSize="110px" fill="#000">
-                Chris
+                Chris Stiles
               </text>
-
-              <text x="0" y="173" fontWeight="900" fontSize="110px" fill="#000">
-                Stiles
-              </text>
-
-              {/* <rect
-                x="700"
-                y="10"
-                // width="100"
-                // height="100"
-                className={styles.rectAnimation}
-              /> */}
             </mask>
 
             <clipPath id="overlay-clip">
@@ -71,75 +56,11 @@ function SVGClip() {
             height="100%"
             x="0"
             y="0"
-            fill="#2a3352"
-            // fill="red"
+            fill={styles.bgColor}
             mask="url(#overlay-mask)"
           />
         </svg>
-
-        {/* <svg width="100%" height="100%" className={styles.overlay}>
-          <rect
-            width="100%"
-            height="100%"
-            fill="#2a3352"
-            mask="url(#overlay-mask)"
-          />
-        </svg> */}
-
-        {/* <svg width="100%" height="100%" className={styles.overlay}>
-          <rect
-            width="100%"
-            height="100%"
-            fill="#fff"
-            mask="url(#overlay-clip)"
-          />
-        </svg> */}
       </div>
-
-      {/* <div
-        className={classNames(styles.clip, styles.bgAnimation)}
-        style={{
-          backgroundImage: `url(${space})`,
-          backgroundAttachment: 'fixed',
-          backgroundRepeat: 'repeat'
-        }}
-      >
-        <svg width="100%" height="100%">
-          <defs>
-            <mask id="svg-clip">
-              <rect x="0" y="0" width="100%" height="100%" fill="#FFF" />
-              <text
-                x="50"
-                y="133"
-                fontWeight="900"
-                fontSize="100px"
-                fill="#000"
-              >
-                SVG Clip
-              </text>
-
-              <rect
-                x="700"
-                y="10"
-                fill="#000"
-                width="200px"
-                height="500px"
-                ref={rectRef}
-                className={styles.rectAnimation}
-              />
-            </mask>
-          </defs>
-
-          <rect
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            fill="#fff"
-            mask="url(#svg-clip)"
-          />
-        </svg>
-      </div> */}
     </React.Fragment>
   );
 }
@@ -147,7 +68,7 @@ function SVGClip() {
 function FixedBackground() {
   return (
     <div className={styles.fixedBgWrapper}>
-      <svg width="100%" height="100%">
+      <svg width="0" height="0">
         <defs>
           <pattern
             id="polka-dots"
@@ -157,17 +78,13 @@ function FixedBackground() {
             height="10"
             patternUnits="userSpaceOnUse"
           >
-            <rect x="3" y="3" width="2" height="2" fill="#000" />
+            <circle cx="2" cy="2" r="1.8" fill={styles.dotColor} />
           </pattern>
         </defs>
+      </svg>
 
-        <rect
-          x="0"
-          y="0"
-          width="100%"
-          height="100%"
-          fill="url(#polka-dots)"
-        ></rect>
+      <svg width="100%" height="100%">
+        <rect x="0" y="0" width="100%" height="100%" fill="url(#polka-dots)" />
       </svg>
     </div>
   );
