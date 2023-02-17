534f99ded25a3385455e8e3c88512f877f69023d
Chris Stiles <chrisstiles92@gmail.com>
2023-02-17 13:18:09 -0800

    Allow variable highlight widths

--- src/app/components/templates/Home/SideProjects/RedditPublishDate/ProjectHighlights/ProjectHighlights.tsx
+++ src/app/components/templates/Home/SideProjects/RedditPublishDate/ProjectHighlights/ProjectHighlights.tsx
@@ -1,44 +1,96 @@
-import { useRef, useEffect, memo } from 'react';
+import { useRef, useState, useEffect, useCallback, memo } from 'react';
 import styles from './ProjectHighlights.module.scss';
+import { useGlobalState } from '@templates/Home';
+import squircleModule from 'css-houdini-squircle/squircle.min.js';
 import gsap from 'gsap';
+import { useInView } from 'react-intersection-observer';
 import { round } from 'lodash';
+import classNames from 'classnames';
 
 export default memo(function ProjectHighlights() {
-  const wrapper = useRef<HTMLDivElement>(null);
+  const wrapper = useRef<any>(null);
   const scroller = useRef<HTMLDivElement>(null);
   const animation = useRef<gsap.core.Tween | null>(null);
+  const [hasStarted, setHasStarted] = useState(false);
+  const { modalIsOpen } = useGlobalState();
+  const { ref, inView } = useInView({
+    fallbackInView: true
+  });
+
+  const setRefs = useCallback(
+    (node: HTMLDivElement) => {
+      wrapper.current = node;
+      ref(node);
+    },
+    [ref]
+  );
 
   useEffect(() => {
-    if (!wrapper.current || !scroller.current?.children.length) return;
+    const init = async () => {
+      await import('css-paint-polyfill');
+      CSS.paintWorklet.addModule(squircleModule);
 
-    const velocity = 25;
-    const elements = Array.from(scroller.current.children);
+      const animate = () => {
+        if (!wrapper.current || !scroller.current?.children.length) {
+          return;
+        }
 
-    const distance = elements.reduce((offset, el) => {
-      gsap.set(el, { x: offset });
-      return offset + el.clientWidth;
-    }, 0);
+        const velocity = 30;
+        const elements = Array.from(scroller.current.children);
+        let maxChildWidth = 0;
 
-    animation.current = gsap.to(scroller.current.children, {
-      x: `+=${distance}`,
-      runBackwards: true,
-      ease: 'none',
-      repeat: -1,
-      duration: round(distance / velocity, 5),
-      modifiers: {
-        x: gsap.utils.unitize(x => parseFloat(x) % distance)
-      }
-    });
+        let distance = elements.reduce((offset, el) => {
+          const width = el.clientWidth;
+          gsap.set(el, { x: offset });
+
+          if (width > maxChildWidth) {
+            maxChildWidth = width;
+          }
+
+          return offset + width;
+        }, 0);
+
+        gsap.set(scroller.current, { left: -maxChildWidth });
+
+        animation.current = gsap.to(elements, {
+          x: `+=${distance}`,
+          runBackwards: true,
+          paused: true,
+          ease: 'none',
+          repeat: -1,
+          duration: round(distance / velocity, 5),
+          modifiers: {
+            x: gsap.utils.unitize(x => parseFloat(x) % distance)
+          }
+        });
+
+        setHasStarted(true);
+      };
+
+      setTimeout(animate, 50);
+    };
+
+    init();
   }, []);
+
+  useEffect(() => {
+    if (!modalIsOpen && inView) {
+      animation.current?.play();
+    } else {
+      animation.current?.pause();
+    }
+  }, [modalIsOpen, inView, hasStarted]);
 
   return (
     <div
-      ref={wrapper}
+      ref={setRefs}
       className={styles.wrapper}
     >
       <div
         ref={scroller}
-        className={styles.scrollWrapper}
+        className={classNames(styles.scrollWrapper, {
+          [styles.hidden]: !hasStarted
+        })}
       >
         <Highlight
           title="1 Highlight box title"
@@ -46,7 +98,7 @@ export default memo(function ProjectHighlights() {
         />
         <Highlight
           title="2 Highlight box title"
-          text="Lorem  dolor sit amet, consectetur"
+          text="Lorem  dolor sit amet, consectetur Lorem  dolor sit amet, consectetur"
         />
         <Highlight
           title="3 Highlight box title"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,11 @@
 import '@styles/main.scss';
 import '@utils/polyfills';
 import SquircleMask from '@images/squircle-mask.svg';
-import { isSSR } from '@helpers';
-import squircleModule from 'css-houdini-squircle/squircle.min.js';
 import 'focus-visible';
 import Head from 'next/head';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-
-if (!isSSR()) {
-  if ('paintWorklet' in CSS) {
-    CSS.paintWorklet.addModule(squircleModule);
-  } else {
-    import('css-paint-polyfill').then(() => {
-      CSS.paintWorklet?.addModule(squircleModule);
-    });
-  }
-}
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page);
